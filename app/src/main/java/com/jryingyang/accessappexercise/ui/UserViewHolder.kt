ecb14d087fa22570a7c5f38d6cbdd0e5282e426c
ChihYing Yang <jrying1992@gmail.com>
2020-09-23 19:17:17 +0800

    Refactor: Use databinding to show list data

--- app/src/main/java/com/jryingyang/accessappexercise/ui/UserViewHolder.kt
+++ app/src/main/java/com/jryingyang/accessappexercise/ui/UserViewHolder.kt
@@ -1,39 +1,23 @@
 package com.jryingyang.accessappexercise.ui
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
-import com.jryingyang.accessappexercise.R
+import com.jryingyang.accessappexercise.databinding.UserItemBinding
 import com.jryingyang.accessappexercise.model.User
 
-class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-    private val login: TextView = view.findViewById(R.id.login)
-    private val avatar: ImageView = view.findViewById(R.id.avatar)
-    private val admin: TextView = view.findViewById(R.id.admin)
+class UserViewHolder(private val binding: UserItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: User) {
-        login.text = item.login
-
-        if (item.site_admin) {
-            admin.visibility = View.VISIBLE
-        }
-
-        Glide.with(itemView).load(item.avatar_url).apply(RequestOptions.circleCropTransform())
-            .diskCacheStrategy(DiskCacheStrategy.ALL).into(avatar)
+        binding.user = item
+        binding.executePendingBindings()
     }
 
     companion object {
         fun create(parent: ViewGroup): UserViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.user_item, parent, false)
-            return UserViewHolder(view)
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = UserItemBinding.inflate(layoutInflater, parent, false)
+            return UserViewHolder(binding)
         }
     }
 }
